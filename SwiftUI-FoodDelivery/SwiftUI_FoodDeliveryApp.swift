0e2f44b80a4e0fb8328fcdeb8c99b43264af2ca3
Pradeep-CG <pradeep.sagar.ios@gmail.com>
2026-07-15 19:42:30 +0530

    Initial Commit

---
+++ SwiftUI-FoodDelivery/SwiftUI_FoodDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FoodDeliveryApp.swift
+//  SwiftUI-FoodDelivery
+//
+//  Created by Pradeep Kumar Sagar on 15/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
